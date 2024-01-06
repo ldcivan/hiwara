70e1a9a1a197f26fcf537cc12549e16241f9141b
Yujio_Nako <69411867+ldcivan@users.noreply.github.com>
2024-01-06 17:51:28 +0800

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -18,7 +18,7 @@ api = 'https://api.iwara.tv'
 
 /*#ifdef H5*/
 files = 'https://back-proxy.pro-ivan.cn/proxy/https://files.iwara.tv'
-api = 'https://back-proxy.pro-ivan.cn/proxy/https://api.iwara.tv'
+api = 'https://awaraapi.pro-ivan.cn/api'
 /*#endif*/
 
 /**API */
